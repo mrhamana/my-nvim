ece15b1c3dce3768d243284eb4089076e59cff8e
mrhamana <saugatkhatiwada80@gmail.com>
2025-06-08 03:21:55 +0545

    added the keymap for the treesitter playground

--- lua/mrhamana/plugins/keystrokes.lua
+++ lua/mrhamana/plugins/keystrokes.lua
@@ -1,1 +1,4 @@
 vim.keymap.set('n','<leader>ff',require('telescope.builtin').find_files,{noremap=true})
+vim.keymap.set("n", "<leader>ts", function()
+  vim.cmd("TSPlaygroundToggle")
+end, { noremap = true, silent = true, desc = "Toggle Tree-sitter Playground" })
