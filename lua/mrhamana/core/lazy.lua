71355856a559c9834ddcb0377d74ca78a4bac48b
mrhamana <saugatkhatiwada80@gmail.com>
2025-06-08 03:16:56 +0545

    added treesitter playground. Visually shows the parsed code

--- lua/mrhamana/core/lazy.lua
+++ lua/mrhamana/core/lazy.lua
@@ -23,7 +23,11 @@ require("lazy").setup({
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = { 'nvim-lua/plenary.nvim' }
     },
-     {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
+     {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false,
+	dependencies = {
+    "nvim-treesitter/playground",
+  },
+     build = ":TSUpdate"},
      {
     "tiagovla/tokyodark.nvim",
     opts = require("mrhamana.plugins.tokyotheme"),
--- lua/mrhamana/plugins/treesitter.lua
+++ lua/mrhamana/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "c", "lua", "rust", "javascript","cpp" },
+  ensure_installed = { "c", "lua", "rust", "javascript","cpp","python" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -9,7 +9,12 @@ require'nvim-treesitter.configs'.setup {
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
 
- 
+playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25,
+        persist_queries = false,
+      }, 
   highlight = {
     enable = true,
 
